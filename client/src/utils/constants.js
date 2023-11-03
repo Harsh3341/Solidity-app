7353df6acd8cb6d493ff3fd4e1223922b931b7c0
Harsh Gupta <harshgupta3341@gmail.com>
2023-11-03 23:59:26 +0530

    transaction deployed

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xB7A3F8848CbcC86DA4f715b32292d47739cA725F";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/VwV_vADAbftrl5K4YhIMwXsLJ3QgmZud",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
